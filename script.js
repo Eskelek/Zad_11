3fe97f109e17718e0d9d8509c1de9d7a593f0c26
Eskel <lorthan966@wp.pl>
2017-06-16 12:57:36 +0200

    fin

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, camera) {
     this.brand = brand;
     this.price = price;
     this.color = color;
+    this.camera = camera;
 };
 Phone.prototype.printInfo = function() {
     console.log("The phone is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var samsungGalaxyS6 = new Phone("Samsung", 1890, "black");
-var onePlusOne = new Phone("OnePlus", 2300, "black");
+Phone.prototype.cameraInfo = function() {
+    console.log("Camera in the " + this.brand + " has " + this.camera + ".")
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver", "12 MPx");
+var samsungGalaxyS6 = new Phone("Samsung", 1890, "black", "16 MPx");
+var onePlusOne = new Phone("OnePlus", 2300, "black", "13 MPx");
 
 iPhone6S.printInfo();
 onePlusOne.printInfo();
 samsungGalaxyS6.printInfo();
+
+samsungGalaxyS6.cameraInfo();
