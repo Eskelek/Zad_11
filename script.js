9e41ddede4c69419ab0109c7bf4067238e99025b
Eskel <lorthan966@wp.pl>
2017-06-18 12:09:43 +0200

    fin

--- script.js
+++ script.js
@@ -68,8 +68,54 @@ $(function() {
             $cardDelete.click(function(){ 
                 self.removeCard(); 
             });
-            //KONSTRUOWANIE KARTY!!!!!!!
-        } 
+            //SKŁADANIE I ZWRACANIE KARTY
+            $card.append($cardDelete) 
+                    .append($cardDescription); 
+            return $card;
+            
+        }
+        Card.prototype = { 
+            removeCard: function() { 
+                this.$element.remove(); 
+            } 
+        }
     }
     
+    var board = { 
+        name: 'Kanban Board', 
+        addColumn: function(column) { 
+            this.$element.append(column.$element); 
+            initSortable(); 
+        }, 
+        $element: $('#board .column-container') 
+    };
+    
+    function initSortable() { 
+        $('.column-card-list').sortable({ 
+            connectWith: '.column-card-list', 
+            placeholder: 'card-placeholder' 
+        }).disableSelection(); 
+    }
+    
+    $('.create-column') 
+        .click(function(){ 
+            var name = prompt('Enter a column name'); 
+            var column = new Column(name); 
+            board.addColumn(column); 
+    });
+    
+    // TWORZENIE KOLUMN 
+    var todoColumn = new Column('To do'); 
+    var doingColumn = new Column('Doing'); 
+    var doneColumn = new Column('Done'); 
+    // DODAWANIE KOLUMN DO TABLICY 
+    board.addColumn(todoColumn); 
+    board.addColumn(doingColumn); 
+    board.addColumn(doneColumn); 
+    // TWORZENIE NOWYCH EGZEMPLARZY KART 
+    var card1 = new Card('New task'); 
+    var card2 = new Card('Create kanban boards'); 
+    // DODAWANIE KART DO KOLUMN 
+    todoColumn.addCard(card1); 
+    doingColumn.addCard(card2);
 });
